d8c44b935bf84f73a5a33ef2c292dd655270a9b7
Andriy <androando777@gmail.com>
2020-10-01 05:53:20 +0300

    Updates

---
+++ precache-manifest.052fac6ab48601146856517955455fcf.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99468c35c5d09d1a4a87ac18851f4e5f",
+    "url": "/werdevs/index.html"
+  },
+  {
+    "revision": "a92b985ff852a1560c86",
+    "url": "/werdevs/static/css/main.bf3b60ff.chunk.css"
+  },
+  {
+    "revision": "6cc8fe917379f055dcbb",
+    "url": "/werdevs/static/js/2.3d7eae61.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/werdevs/static/js/2.3d7eae61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a92b985ff852a1560c86",
+    "url": "/werdevs/static/js/main.bfd6d40c.chunk.js"
+  },
+  {
+    "revision": "29099020ac37aad61b84",
+    "url": "/werdevs/static/js/runtime-main.70eb1463.js"
+  },
+  {
+    "revision": "b1a38376a7a592cc41e7b6701aada3d1",
+    "url": "/werdevs/static/media/Logo original RGB.b1a38376.svg"
+  },
+  {
+    "revision": "36f38cf6ac8fe12445973ede01747894",
+    "url": "/werdevs/static/media/keyboard_arrow_left.36f38cf6.svg"
+  },
+  {
+    "revision": "135d588a15732fdf0a67638541c8a1bc",
+    "url": "/werdevs/static/media/keyboard_arrow_right.135d588a.svg"
+  },
+  {
+    "revision": "c2f93ce39bc18a356b7d7ab04d6819b4",
+    "url": "/werdevs/static/media/layer.c2f93ce3.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f23a5e35b4675a865ea59979c3ea0f10.js"
+  "/werdevs/precache-manifest.052fac6ab48601146856517955455fcf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/werdevs/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
