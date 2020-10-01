fbe196e3bcc0075afa984f4eda38f0967fb2ea89
Andriy <androando777@gmail.com>
2020-10-01 04:09:38 +0300

    Updates

---
+++ precache-manifest.9b1046a86e4ebf521af861ce4c4f60c7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbde045067faf91d4b03e6e7d5089589",
+    "url": "/werdevs/index.html"
+  },
+  {
+    "revision": "e47509cf383d46749571",
+    "url": "/werdevs/static/css/main.28ca8c86.chunk.css"
+  },
+  {
+    "revision": "e724b8125197e8232f5a",
+    "url": "/werdevs/static/js/2.04f5e03d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/werdevs/static/js/2.04f5e03d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e47509cf383d46749571",
+    "url": "/werdevs/static/js/main.249aacef.chunk.js"
+  },
+  {
+    "revision": "29099020ac37aad61b84",
+    "url": "/werdevs/static/js/runtime-main.70eb1463.js"
+  },
+  {
+    "revision": "b1a38376a7a592cc41e7b6701aada3d1",
+    "url": "/werdevs/static/media/Logo original RGB.b1a38376.svg"
+  },
+  {
+    "revision": "36f38cf6ac8fe12445973ede01747894",
+    "url": "/werdevs/static/media/keyboard_arrow_left.36f38cf6.svg"
+  },
+  {
+    "revision": "135d588a15732fdf0a67638541c8a1bc",
+    "url": "/werdevs/static/media/keyboard_arrow_right.135d588a.svg"
+  },
+  {
+    "revision": "c2f93ce39bc18a356b7d7ab04d6819b4",
+    "url": "/werdevs/static/media/layer.c2f93ce3.png"
+  }
+]);
